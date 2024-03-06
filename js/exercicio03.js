f4cc414ea7b516c5def0244a7510e173e7f06cda
gabriel-php <gabriel.javax@gmail.com>
2024-03-06 18:46:01 -0300

    Falta fazer o exercicio 3

--- js/exercicio03.js
+++ js/exercicio03.js
@@ -1,12 +1,1 @@
-
-var diasSemana = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
-
-
-var dataAtual = new Date();
-
-var numeroDiaSemana = dataAtual.getDay();
-
-var diaSemanaPorExtenso = diasSemana[numeroDiaSemana];
-
-alert("Hoje é " + diaSemanaPorExtenso);
-console.log("Hoje é " + diaSemanaPorExtenso);
+alert("Falta fazer esse exercício");
